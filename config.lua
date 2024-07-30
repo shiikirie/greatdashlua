99debe63c21f747384b2a56636daaf1ad04c9267
shiikirai <shiikiriedev@gmail.com>
2024-07-30 18:33:35 +0300

    Create config.lua
    ReplicatedStorage/Modules/Dash/Actor/Config

---
+++ config.lua
@@ -0,0 +1,9 @@
+return {Speed = 0.4, CD = 1.5, Lenght = 30, 
+	{--default dash anims
+		W = 'rbxassetid://13833333834',
+		A = 'rbxassetid://13833396892',
+		S = 'rbxassetid://13833384868',
+		D = 'rbxassetid://13833390791',
+	}
+}
+--dash by shikirai - https://www.youtube.com/@shikirai
